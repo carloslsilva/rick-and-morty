6372a8660b24e7a2cc2c029e590506d3b36273a9
Carlos Silva <clsilva78@gmail.com>
2023-11-30 15:28:50 -0500

    First commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx'],
+  endOfLine: 'lf',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  quoteProps: 'as-needed',
+  trailingComma: 'none',
+  htmlWhitespaceSensitivity: 'css',
+  proseWrap: 'preserve'
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl'>Hello There!</h1>
+    </>
+  )
+}
+
+export default App
