cefdf6ef0b18838b308c726f211f174452093c05
Carlos Silva <clsilva78@gmail.com>
2024-01-12 02:56:55 -0500

    added link to character page into home page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,66 @@
+import {
+  faGithub,
+  faLinkedin,
+  faXTwitter
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { FC } from 'react'
 import { Container } from './ui/Container'
 
+const contacts = [
+  {
+    href: 'https://www.linkedin.com/in/carloslsilva/',
+    label: 'LinkedIn',
+    icon: faLinkedin
+  },
+  {
+    href: 'https://github.com/carloslsilva/',
+    label: 'GitHub',
+    icon: faGithub
+  },
+  {
+    href: 'https://twitter.com/carloslsilva',
+    label: 'Twitter',
+    icon: faXTwitter
+  }
+]
+
 export const Footer: FC = () => (
-  <Container variant='long' as='footer'>
-    <p>© 2021</p>
+  <Container
+    className='flex flex-row items-center justify-between gap-8 py-16'
+    variant='long'
+    as='footer'
+  >
+    <span className='flex items-center justify-center gap-4'>
+      <span className='flex items-center justify-center gap-2'>
+        {contacts.map(contact => (
+          <a
+            className='duration-300 ease-out hover:-translate-y-1 hover:text-amber-500 active:animate-ping'
+            key={contact.label}
+            href={contact.href}
+            rel='noreferrer'
+            target='_blank'
+          >
+            <FontAwesomeIcon icon={contact.icon} />
+          </a>
+        ))}
+      </span>
+      <a
+        className='hover:text-amber-500'
+        href='https://www.carloslsilva.com/'
+        rel='noreferrer'
+        target='_blank'
+      >
+        @carloslsilva
+      </a>
+    </span>
+    <a
+      className='duration-300 ease-out hover:text-amber-500 active:animate-ping'
+      href='https://rickandmortyapi.com/'
+      rel='noreferrer'
+      target='_blank'
+    >
+      Powered by Rick and Morty API
+    </a>
   </Container>
 )
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,4 +1,6 @@
-import { useLoaderData } from 'react-router-dom'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link, useLoaderData } from 'react-router-dom'
 import { Card } from '../components/ui/Card'
 import { Container } from '../components/ui/Container'
 import { Character } from '../lib/types'
@@ -15,12 +17,25 @@ export default function Root() {
       >
         <h1 className='text-9xl font-bold'>Rick & Morty</h1>
       </Container>
-      <Container className='py-24' variant='long' as='section'>
+      <Container className='pb-8 pt-24' variant='long' as='section'>
         <div className='grid grid-cols-1 items-center justify-center gap-8 md:grid-cols-2 xl:grid-cols-3'>
           {characters.map(character => (
             <Card key={character.id} character={character} link />
           ))}
         </div>
+      </Container>
+      <Container
+        className='flex items-center justify-end py-4'
+        variant='long'
+        as='header'
+      >
+        <Link
+          className='flex items-center justify-center gap-2 hover:text-amber-500 active:animate-ping'
+          to='/characters'
+        >
+          <span>Explore all</span>
+          <FontAwesomeIcon icon={faSearch} size='lg' />
+        </Link>
       </Container>
     </>
   )
