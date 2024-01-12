470c1ecc23211c7d48f98e664c7b71b71a35999d
Carlos Silva <clsilva78@gmail.com>
2024-01-12 12:06:17 -0500

    Responsive styles for search

--- src/components/ui/SearchForm.tsx
+++ src/components/ui/SearchForm.tsx
@@ -1,63 +1,56 @@
 import { faSearch, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import clsx from 'clsx'
 import { useSearchForm } from '../../hooks/useSearchForm'
 
 export const SearchForm = () => {
   const { onSubmit, onReset, register } = useSearchForm()
 
   return (
-    <form className='flex flex-row' onSubmit={onSubmit}>
+    <form className={clsx('flex flex-col', 'sm:flex-row')} onSubmit={onSubmit}>
       <input
-        className='flex-grow rounded-l-lg bg-primary-200 px-4 py-2 text-primary-500 outline-none focus:bg-primary-100'
+        className={clsx(
+          'flex-grow rounded-t-lg bg-primary-200 px-4 py-2 text-primary-500 outline-none focus:bg-primary-100',
+          'sm:rounded-none sm:rounded-l-lg'
+        )}
         placeholder='Search by name...'
         autoComplete='off'
         {...register('name')}
       />
       <select
-        className='appearance-none bg-primary-200 p-2 text-primary-600 outline-none hover:bg-primary-300'
+        className='appearance-none bg-primary-200 p-2 text-primary-500 outline-none hover:bg-primary-300'
         {...register('status')}
       >
-        <option className='' value=''>
-          Status...
-        </option>
-        <option className='' value='alive'>
-          Alive
-        </option>
-        <option className='' value='dead'>
-          Dead
-        </option>
-        <option className='' value='unknown'>
-          Unknown
-        </option>
+        <option value=''>Status...</option>
+        <option value='alive'>Alive</option>
+        <option value='dead'>Dead</option>
+        <option value='unknown'>Unknown</option>
       </select>
       <select
-        className='appearance-none bg-primary-200 p-2 text-primary-600 outline-none hover:bg-primary-300'
+        className='appearance-none bg-primary-200 p-2 text-primary-500 outline-none hover:bg-primary-300'
         {...register('gender')}
       >
-        <option className='' value=''>
-          Gender...
-        </option>
-        <option className='' value='male'>
-          Male
-        </option>
-        <option className='' value='female'>
-          Female
-        </option>
-        <option className='' value='genderless'>
-          Genderless
-        </option>
-        <option className='' value='unknown'>
-          Unknown
-        </option>
+        <option value=''>Gender...</option>
+        <option value='male'>Male</option>
+        <option value='female'>Female</option>
+        <option value='genderless'>Genderless</option>
+        <option value='unknown'>Unknown</option>
       </select>
       <button
-        className='inline-flex w-12 items-center justify-center bg-red-700 text-primary-100 hover:bg-red-800'
+        className={clsx(
+          'h-8 bg-red-700 text-primary-100 hover:bg-red-800',
+          'sm:h-auto sm:w-12'
+        )}
         onClick={onReset}
       >
         <FontAwesomeIcon icon={faTrash} />
       </button>
       <button
-        className='inline-flex w-12 items-center justify-center rounded-r-lg bg-amber-500 text-primary-100 hover:bg-amber-600'
+        className={clsx(
+          'h-8 bg-amber-500 text-primary-100 hover:bg-amber-600',
+          'rounded-b-lg',
+          'sm:h-auto sm:w-12 sm:rounded-none sm:rounded-r-lg'
+        )}
         type='submit'
       >
         <FontAwesomeIcon icon={faSearch} />
